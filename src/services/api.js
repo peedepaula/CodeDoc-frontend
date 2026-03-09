f438ef56d72ceacf69d5b75ae2657c4de78925db
peedepaula <peedepaula13@gmail.com>
2026-03-09 14:43:02 -0300

    Frontend concluído

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,14 @@
-// src/services/api.js
 import axios from "axios";
 
 const TOKEN_KEY = "token_sufla";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.102:8000",
+  baseURL: "http://192.168.0.105:8000",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Interceptor para adicionar token automaticamente
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem(TOKEN_KEY);
   if (token) {
@@ -20,4 +18,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
-export { TOKEN_KEY }; // exporta a chave para usar no componente
+export { TOKEN_KEY };
