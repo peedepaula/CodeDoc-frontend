719ad76d92655db37da468ac61bdf1add42732db
peedepaula <peedepaula13@gmail.com>
2026-05-06 15:40:00 -0300

    Mais um teste de build.

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@
 <script>
 import Historico from '@/components/Historico.vue';
 import PopUpGlobal from '@/components/PopUps/PopUpGlobal.vue';
-import CopiadoComp from '@/components/popUps/Copiado.vue';
+import CopiadoComp from '@/components/PopUps/Copiado.vue';
 
 export default{
     name: "App",
