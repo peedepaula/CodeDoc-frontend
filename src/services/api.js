e555766a49e27266505b7f3e50a498bca93f53a7
peedepaula <peedepaula13@gmail.com>
2026-05-06 16:17:38 -0300

    Teste rodando real

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const TOKEN_KEY = "token_sufla";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.103:8000",
+  baseURL: "https://codedoc-backend-jmym.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
